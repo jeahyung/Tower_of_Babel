b2ca9ffb891dbae700693a311c9ea0e21157968e
jeahyung <122067674+jeahyung@users.noreply.github.com>
2024-05-28 15:49:11 +0900

    soundTest Sene

--- Assets/pjh/Script/Monster/TraceMonsterMovement.cs
+++ Assets/pjh/Script/Monster/TraceMonsterMovement.cs
@@ -118,7 +118,7 @@ public class TraceMonsterMovement : MonoBehaviour
 
         SetPosition(nextPos);
 
-        AudioManager.instance.PlaySfx(AudioManager.Sfx.Monster_Move);
+      //  AudioManager.instance.PlaySfx(AudioManager.Sfx.Monster_Move);
 
     }
 
