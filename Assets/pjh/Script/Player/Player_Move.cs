a2ad4043ac18e407ea3b9fa06903fb6d660f4a39
jeahyung <122067674+jeahyung@users.noreply.github.com>
2024-06-04 00:47:08 +0900

    player_move

--- Assets/pjh/Script/Player/Player_Move.cs
+++ Assets/pjh/Script/Player/Player_Move.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour
 {
     private Rigidbody rigid;
-
+    private Animator ani;
     private PlayerMovement player;
     private bool control = true;
     private AudioSource audio;
@@ -15,6 +15,7 @@ public class Player_Move : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
         player = GetComponent<PlayerMovement>();
         audio = GetComponent<AudioSource>();
+        ani = GetComponentInChildren<Animator>();
     }
 
    
@@ -29,16 +30,29 @@ public class Player_Move : MonoBehaviour
         if (control == false) { return; }
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
+        
         rigid.velocity = new Vector3(x * 5, rigid.velocity.y, z * 5);
+        Vector3 movement = new Vector3(x, 0, z).normalized * 5;
+
         if (rigid.velocity.x != 0 || rigid.velocity.z != 0)
         {
+            ani.SetBool("isRun", true);
             if (!audio.isPlaying)
             {
                 audio.Play();
             }
         }
         else
+        {
             audio.Stop();
+            ani.SetBool("isRun", false);
+        }
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 5 * Time.deltaTime * 100);
+        }
         //if (rigid.velocity.x != 0 || rigid.velocity.z != 0)
         //{
         //    if (!AudioManager.instance.CheckAudio(AudioManager.Sfx.Player_Walk))
