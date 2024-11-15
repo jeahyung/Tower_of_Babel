7d760fc5d2bc81827f5b5ad2d53a6ac0f33247c8
Atara02 <yeojp73@gmail.com>
2024-11-15 23:21:25 +0900

    Revert "상자 버그 막기"
    This reverts commit 3b9aa02aaba12e2cf62e66227127f755cbebb57c.

--- Assets/ysb/New/Scripts/Mob/MobManager.cs
+++ Assets/ysb/New/Scripts/Mob/MobManager.cs
@@ -124,9 +124,8 @@ public class MobManager : MonoBehaviour//Singleton<MobManager>
             }
             if (isKey || isKey2 || isUseItem)
             {
-                bool isCancel = map.ShowPlayerTile();
-                if (isCancel == false) { return; }
                 isKey = isKey2 = isUseItem = false;
+                map.ShowPlayerTile();
                 //AnotherBtnActive.Instance.EnableUIInteraction();
                 //BtnOff.Instance.EnableUIInteraction();
             }
